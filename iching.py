21822c63eeb67b27321cbb1afa242a0d52329048
Aleksey I. Ozimkov <info@7logic.ru>
2021-03-17 20:07:22 +0300

    planning the game

---
+++ iching.py
@@ -0,0 +1,19 @@
+def main():
+    # Print out the game introducing
+
+    # Get user name and age
+
+    # Game Loop
+
+    # # Print a start message
+
+    # # Toss a coin six time
+
+    # # Draw the Hexagram
+
+    # # Print out information about this Hexagram from the Book of Changes
+
+    # # User dialog 
+    
+if __name__ == "__main__":
+    main()
