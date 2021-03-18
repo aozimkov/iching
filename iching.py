389d130f2e0bee88269d82075475b0065ece477c
Aleksey I. Ozimkov <info@7logic.ru>
2021-03-18 08:43:45 +0300

    Code refactored, hexagram printing error fixed. Hexagram class updated. Dicts simplified.

--- iching.py
+++ iching.py
@@ -4,47 +4,46 @@ from dicts import *
 class Hexagram:
     def __init__(self, toss):
         self.toss = toss
+        self.hexagram_key = self.get_hexagram_binary_key()
 
     def __repr__(self):
-        for side in self.get_hexagram_binary_string():
-            if side == '1':
-                print("_________")
-            else:
-                print("___   ___")
-        return hexagrams[self.get_hexagram_number()-1]
+        print(self.get_hexagram_picture())
+        return self.get_hexagram_description()
 
-    def get_hexagram_binary_string(self):
+    def get_hexagram_binary_key(self):
         binary_hexagram_sum = bin(self.toss)
         key_without_0b = str(binary_hexagram_sum)[2:]
         ready_binary_key = "{0:0>6}".format(key_without_0b)
         return ready_binary_key
 
-    def get_hexagram_number(self):
-        return hexagon_numbers_dict[self.get_hexagram_binary_string()]
+    def get_hexagram_description(self):
+        return hexagrams_dict[self.hexagram_key]
+    
+    def get_hexagram_picture(self):
+        hexagram_picture = ''
+        binary_key = self.hexagram_key
+        for symbol in reversed(binary_key):
+            if symbol == '1':
+                hexagram_picture += "_________\n"
+            else:
+                hexagram_picture += "___   ___\n"
+        return hexagram_picture
 
 
 def main():
 
-    # Print out the game introducing
     print_introduce()
-
-    # Get user name
     user_name = str(input("Please enter your name: "))
 
     # Game Loop
     while True:
 
-        # # Print a start message
         input("Hi, {user}! Please enter your question here or ask it in your mind\nand press ENTER button to start iChing:".format(user=user_name))
 
-        # # Generate and draw the Hexagram
         hexagram = Hexagram(toss())
-        
-        # # Print out information about this Hexagram from the Book of Changes
         print(hexagram)
 
-        # # User dialog 
-        next_action = input("Press Enter to ask one more question or enter 'exit' and push Enter to close the program")
+        next_action = input("Press Enter to ask one more question or enter 'exit' and push Enter to close the program\n")
 
         if next_action.lower() == 'exit':
             print('Bye-bye!')
